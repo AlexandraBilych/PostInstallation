ee4901fe555e71b10a4b5136f5717176bf3fbf54
Olexandra Bilych <lyubasovasasha@gmail.com>
2017-01-11 18:07:38 +0200

    Add sublime3 and basic update
    Signed-off-by: Olexandra Bilych <lyubasovasasha@gmail.com>

--- postInstall.sh
+++ postInstall.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+#add repositories
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+
+# basic update
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+#install apps
+sudo apt install \
+sublime-text-installer
+
