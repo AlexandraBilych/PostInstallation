404a36cf693b4d599e7147954b516a822b19f5c2
Olexandra Bilych <lyubasovasasha@gmail.com>
2017-01-12 14:52:01 +0200

    Add commands for skypeforlinux
    Signed-off-by: Olexandra Bilych <lyubasovasasha@gmail.com>

--- postInstall.sh
+++ postInstall.sh
@@ -3,11 +3,19 @@
 #add repositories
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 
+#install skypeforlinux
+curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
+sudo apt -y update
+sudo apt install apt-transport-https -y
+sudo tee /etc/apt/sources.list.d/skypeforlinux.list
+
 # basic update
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
 #install apps
 sudo apt install \
-sublime-text-installer
+apt-transport-https\
+sublime-text-installer \
+skypeforlinux
 
